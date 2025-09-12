1e576a05b050b51ba9a951ec14ec7482064fcfa6
power2loop <kumar.vkvikash3@gmail.com>
2025-09-13 00:54:47 +0530

    homepage error

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { SidebarDemo } from '../../components/Sidebar/Sidebar'
 import './Home.css'
 
 const Home = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className="h-screen w-full flex items-center justify-center bg-gray-800">
+      <SidebarDemo />
+    </div>
   )
 }
 
--- Frontend/src/pages/LandingPage/LandingPage.jsx
+++ Frontend/src/pages/LandingPage/LandingPage.jsx
@@ -5,16 +5,14 @@ import Footer from '../../components/Footer/Footer';
 import Hero from '../../components/Hero/Hero';
 import Navbar from '../../components/Navbar/Navbar';
 import './LandingPage.css'
-import { SmoothCursor } from '../../components/ui/smooth-cursor';
 
 const Home = () => {
   return (
     <>
-        <SmoothCursor/>
-        <Navbar />
-        <Hero />
-        <Features />
-        <CTA />
+      <Navbar />
+      <Hero />
+      <Features />
+      <CTA />
       <Footer />
     </>
   )
