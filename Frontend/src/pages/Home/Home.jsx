5d2f7fd7c1755211c75fd250197667fb6d123bf8
Aryan Gupta <Aryangupta.x01@gmail.com>
2025-09-13 19:18:47 +0530

    h

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className="Home-Container">
       <Sidebar />
       <div className="main-content">
-        <div className="card-small" id="greet"><p>Hello,<br/><span>Aryan Gupta</span></p></div>
+        <div className="card-small" id="greet"><p>Hello,<br/><span className="gradient-text">Aryan Gupta</span></p></div>
         <div className="card-small">Card 2</div>
         <div className="card-small">Card 3</div>
         <div className="card-large">Big Bottom Card</div>
