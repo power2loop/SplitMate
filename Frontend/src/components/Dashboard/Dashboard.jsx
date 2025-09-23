9a74e13dc0d8982b83c2ad0731b85e916c74ef0f
Aryan Gupta <Aryangupta.x01@gmail.com>
2025-09-23 17:06:22 +0530

    Update Dashboard.jsx

--- Frontend/src/components/Dashboard/Dashboard.jsx
+++ Frontend/src/components/Dashboard/Dashboard.jsx
@@ -3,17 +3,15 @@ import Chart from "../Chart/Chart";
 import './Dashboard.css'
 import ExpenseCard from '../ExpenseCard/ExpenseCard';
 import WalletCard from '../WalletCard/WalletCard';
-import { useAuth } from '../../Context/AuthContext.js'; // custom hook from context
 
 const Dashboard = () => {
-  const { user, loading } = useAuth();
-  const name = loading ? '...' : (user?.username || 'Friend');
+  
 
   return (
     <div className="main-content">
       <div className="cards">
         <div className="card-small" id="greet">
-          <p>Hello,<br /><span className="gradient-text">{name}</span></p>
+          <p>Hello,<br /><span className="gradient-text">Aryan Gupta</span></p>
         </div>
         <div className="card-small"><ExpenseCard /></div>
         <div className="card-small"><WalletCard /></div>
