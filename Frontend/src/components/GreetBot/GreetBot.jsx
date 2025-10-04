3299d5e3f13f3ee1830960107779d44a917976ab
power2loop <kumar.vkvikash3@gmail.com>
2025-10-04 16:28:25 +0530

    ..

--- Frontend/src/components/GreetBot/GreetBot.jsx
+++ Frontend/src/components/GreetBot/GreetBot.jsx
@@ -1,7 +1,7 @@
 import Spline from '@splinetool/react-spline';
 import "./GreetBot.css"
 
-export default function App() {
+export default function GreetBot() {
   return (
     <Spline scene="https://prod.spline.design/VpyMiS8aigyDBGoo/scene.splinecode" />
   );
