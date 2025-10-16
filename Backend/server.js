f2814dad21c6cc6a7ecaef1a75f1e073fe636696
power2loop <kumar.vkvikash3@gmail.com>
2025-10-17 02:13:04 +0530

    fix

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,9 @@ import 'dotenv/config.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import UserRoutes from './src/routes/UserRoutes.js';
-import GroupRoutes from './src/routes/groupRoutes.js';
+import GroupRoutes from './src/routes/GroupRoutes.js';
 import { connectDB } from './src/config/db.js';
-import expenseRoutes from "./src/routes/expenseRoutes.js";
+import expenseRoutes from "./src/routes/ExpenseRoutes.js";
 import path from "path";
 import { fileURLToPath } from 'url';
 
@@ -27,6 +27,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Frontend/dist/index.html"));
+})
 app.get('/', (_req, res) => res.send('API is running...'));
 
 app.use('/api/users', UserRoutes);
