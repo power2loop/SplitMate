e0c75f4c106c7f973a9b61e5d350240fbfb9b9ee
power2loop <kumar.vkvikash3@gmail.com>
2025-10-17 09:44:12 +0530

    fix again

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import 'dotenv/config.js';
 import cors from 'cors';
@@ -7,16 +6,11 @@ import UserRoutes from './src/routes/UserRoutes.js';
 import GroupRoutes from './src/routes/groupRoutes.js';
 import { connectDB } from './src/config/db.js';
 import expenseRoutes from "./src/routes/expenseRoutes.js";
-import path from "path";
-import { fileURLToPath } from 'url';
-
 
 const app = express();
 connectDB();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-const FrontendUrl = "http://localhost:5173"
+const FrontendUrl = "http://localhost:5317"
 
 app.use(cors({
     origin: FrontendUrl,
@@ -27,15 +21,11 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '../Frontend/dist')));
-app.use("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/Frontend/dist/index.html"));
-})
+// API routes first
 app.get('/', (_req, res) => res.send('API is running...'));
-
 app.use('/api/users', UserRoutes);
 app.use('/api/groups', GroupRoutes);
-app.use("/api/expenses", expenseRoutes)
+app.use("/api/expenses", expenseRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
