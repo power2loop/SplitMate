aabc2788acb83c026e52e606d976f649fad90d0f
power2loop <kumar.vkvikash3@gmail.com>
2025-10-20 00:58:26 +0530

    firebase fix

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -1,14 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FB_apiKey,
+//   authDomain: import.meta.env.VITE_FB_authDomain,
+//   projectId: import.meta.env.VITE_FB_projectId,
+//   storageBucket: import.meta.env.VITE_FB_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_FB_messagingSenderId,
+//   appId: import.meta.env.VITE_FB_appId,
+//   measurementId: import.meta.env.VITE_FB_measurementId,
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FB_apiKey,
-  authDomain: import.meta.env.VITE_FB_authDomain,
-  projectId: import.meta.env.VITE_FB_projectId,
-  storageBucket: import.meta.env.VITE_FB_storageBucket,
-  messagingSenderId: import.meta.env.VITE_FB_messagingSenderId,
-  appId: import.meta.env.VITE_FB_appId,
-  measurementId: import.meta.env.VITE_FB_measurementId,
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
+  measurementId: import.meta.env.VITE_FB_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
